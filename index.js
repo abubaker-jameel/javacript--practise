36cabb9f595f9b8414ee5b3b4b4f38cb2df087ee
abubaker <codecookies958@gmail.com>
2023-01-01 01:01:12 +0500

    Variables

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 // javaScript Practice
 
+// Variables
+
+// declare using const
+
+const a = 10; //const is a block const meaning constant value which cant be update
+var b = 10; //var is used to store variable it is not consider good value because it cause issues
+let c = 10; //let is a block level variable which can be update
+// we can also declare a variable using just identifiers / name
+
